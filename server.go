068728ef77815b384b018f97e18e06221700f1cb
Eric Putnam <putnam.eric@gmail.com>
2021-11-06 21:30:28 -0700

    Create first surveys API with gin
    Currently, this server is just a boring old server that does...not much. This change implements a small surveys API using the gin library and this tutorial; https://golang.org/doc/tutorial/web-service-gin . This is the next step towards the health check app. Next steps include adding a database and https.

--- server.go
+++ server.go
@@ -1,36 +1,89 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"strconv"
-	"sync"
+
+	"github.com/gin-gonic/gin"
 )
 
-var counter int
-var mutex = &sync.Mutex{}
-
-func echoString(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello")
+type answer struct {
+	QuestionID string `json:"questionId"`
+	Answer     int    `json:"answer"`
 }
 
-func incrementCounter(w http.ResponseWriter, r *http.Request) {
-	mutex.Lock()
-	counter++
-	fmt.Fprintf(w, strconv.Itoa(counter))
-	mutex.Unlock()
+type surveyResponse struct {
+	ID      string   `json:"id"`
+	Answers []answer `json:"answers"`
 }
+
+type survey struct {
+	ID        string           `json:"id"`
+	Team      string           `json:"team"`
+	Responses []surveyResponse `json:"responses"`
+}
+
+var surveys = []survey{
+	{ID: "1", Team: "CD4PE", Responses: nil},
+	{ID: "2", Team: "Froyo", Responses: nil},
+	{ID: "3", Team: "Cygnus", Responses: nil},
+}
+
+/*
+    POST /surveys
+    	creates a survey for cd4pe
+		creates a survey URI
+
+	POST /surveys/response?survey={surveyId}
+		creates a survey response
+*/
 
 func main() {
 
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	router := gin.Default()
+	router.GET("/surveys", getSurveys)
+	router.POST("/surveys", postSurvey)
 
-	http.HandleFunc("/increment", incrementCounter)
+	router.Run("localhost:8080")
+}
 
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
+func getSurveys(c *gin.Context) {
+	if c.Query("team") != "" {
+		team := c.Query("team")
 
-	log.Fatal(http.ListenAndServeTLS(":443", "server.crt", "server.key", nil))
+		for _, s := range surveys {
+			if s.Team == team {
+				c.IndentedJSON(http.StatusOK, s)
+				return
+			}
+		}
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "survey not found"})
+	} else {
+		c.IndentedJSON(http.StatusOK, surveys)
+	}
+}
+
+func postSurvey(c *gin.Context) {
+	var newSurvey survey
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newSurvey); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	surveys = append(surveys, newSurvey)
+	c.IndentedJSON(http.StatusCreated, newSurvey)
+}
+
+func getSurveyByTeam(c *gin.Context) {
+	team := c.Query("team")
+
+	for _, s := range surveys {
+		if s.Team == team {
+			c.IndentedJSON(http.StatusOK, s)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "survey not found"})
 }
